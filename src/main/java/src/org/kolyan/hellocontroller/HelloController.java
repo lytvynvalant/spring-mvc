99f3c1789255ec50d2bcd54444aed9bccd94f15b
Mykolai_Lytvyn <Mykolai_Lytvyn@epam.com>
2016-06-21 12:35:05 +0300

    MultiAction Controller class, @RequestMapping annotation at class level.

--- src/main/java/src/org/kolyan/hellocontroller/HelloController.java
+++ src/main/java/src/org/kolyan/hellocontroller/HelloController.java
@@ -10,13 +10,27 @@ import org.springframework.web.servlet.ModelAndView;
  */
 // define entity using annotation
 @Controller
+//@RequestMapping -> annotation  at class level mean that mapping of class  is concatenated with mapping of method
+@RequestMapping("/greeting")
 public class HelloController {
+
     // define mapping using annotation
+    // url "http://localhost:8080/spring_mvc/greeting/welcome"
     @RequestMapping("/welcome")
     protected ModelAndView hellowWorld() {
         // modelAndView -> define page
         ModelAndView modelAndView = new ModelAndView("HelloPage");
         modelAndView.addObject("welcomeMessage", "He User, welcome to the first Spring MVC Application");
+        return modelAndView;
+    }
+
+    // define mapping using annotation
+    // url "http://localhost:8080/spring_mvc/greeting/hi"
+    @RequestMapping("/hi")
+    protected ModelAndView hiWorld() {
+        // modelAndView -> define page
+        ModelAndView modelAndView = new ModelAndView("HelloPage");
+        modelAndView.addObject("welcomeMessage", "HI!!!");
         return modelAndView;
     }
 }

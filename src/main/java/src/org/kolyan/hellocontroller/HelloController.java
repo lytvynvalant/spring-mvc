7591c0603313eb12d52e6b6de0b5e03ef347284d
Mykolai_Lytvyn <Mykolai_Lytvyn@epam.com>
2016-06-21 12:39:08 +0300

    Minor changes.

--- src/main/java/src/org/kolyan/hellocontroller/HelloController.java
+++ src/main/java/src/org/kolyan/hellocontroller/HelloController.java
@@ -5,32 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * class witch it the controller in Spring MVC must extend 'AbstractController.class'
+ * class witch is the controller in Spring MVC must extend 'AbstractController.class'
  * or using annotation '@Controller'
  */
-// define entity using annotation
 @Controller
-//@RequestMapping -> annotation  at class level mean that mapping of class  is concatenated with mapping of method
-@RequestMapping("/greeting")
 public class HelloController {
-
-    // define mapping using annotation
-    // url "http://localhost:8080/spring_mvc/greeting/welcome"
     @RequestMapping("/welcome")
     protected ModelAndView hellowWorld() {
         // modelAndView -> define page
         ModelAndView modelAndView = new ModelAndView("HelloPage");
         modelAndView.addObject("welcomeMessage", "He User, welcome to the first Spring MVC Application");
-        return modelAndView;
-    }
 
-    // define mapping using annotation
-    // url "http://localhost:8080/spring_mvc/greeting/hi"
-    @RequestMapping("/hi")
-    protected ModelAndView hiWorld() {
-        // modelAndView -> define page
-        ModelAndView modelAndView = new ModelAndView("HelloPage");
-        modelAndView.addObject("welcomeMessage", "HI!!!");
         return modelAndView;
     }
 }

049d7c20d10c3a17a616ceb8081e26235183a3e7
Mykolai_Lytvyn <Mykolai_Lytvyn@epam.com>
2016-06-21 15:45:42 +0300

    Handling an HTML form using @RequestParam annotation.

---
+++ src/main/java/src/org/kolyan/hellocontroller/StudenAdmissionController.java
@@ -0,0 +1,27 @@
+package src.org.kolyan.hellocontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class StudenAdmissionController {
+
+    // url "http://localhost:8080/spring_mvc/addmissionForm.html"
+    @RequestMapping(value = "addmissionForm.html", method = RequestMethod.GET)
+    public ModelAndView getAdmissionForm() {
+        ModelAndView modelAndView = new ModelAndView("AdmissionForm");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "submitForm.html", method = RequestMethod.POST)
+    public ModelAndView submitAdmissionForm(@RequestParam("studentName") String name, @RequestParam("studentHobby") String studentHobby) {
+        ModelAndView modelAndView = new ModelAndView("AdmissionSucces");
+        modelAndView.addObject("msg", "Details submitted by you:<br> name:" + name + ", " + "hobby: " + studentHobby);
+        return modelAndView;
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/AdmissionForm.jsp
@@ -0,0 +1,12 @@
+<h1>Studen admission form for engineering courses</h1>
+<%-- action -> it is 'url' where will be submitted form
+  post -> form's method --%>
+<form action="/spring_mvc/submitForm.html" method="post">
+    <p>
+        Student's name <input type="text" name="studentName">
+    </p>
+    <p>
+        Student's name <input type="text" name="studentHobby">
+    </p>
+    <input type="submit" name="Submit this form be clicking here">
+</form>
